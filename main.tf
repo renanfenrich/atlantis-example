834d2b1a05434da5b8d06d3afc5364de2a301883
Renan Fenrich <renan.fenrich@gmail.com>
2025-09-24 00:58:03 -0300

    Update main.tf
    Signed-off-by: Renan Fenrich <renan.fenrich@gmail.com>

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "null_resource" "another_example" {
+  provisioner "local-exec" {
+    command = "echo This is another example."
+  }
+}
+
 resource "null_resource" "example" {
   provisioner "local-exec" {
     command = "echo Hello, World!"
